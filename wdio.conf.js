7208917510b406eda8282bacfa3059d16d54592d
Ramon <ramoncajueiro@me.com>
2024-07-16 23:44:52 -0300

    Teste Mobile

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,20 +1,31 @@
 export const config = {
-    runner: 'local',
-    port: 4723,
+    // runner: 'local',
+    // port: 4723,
+
+    user: 'RSCC',
+    key: '8920fcf1-7c1b-4ff2-8311-2776e503d07d',
+    hostname: 'ondemand.us-west-1.saucelabs.com',
+    port: 443,
+    path: '/wd/hub',
+
     specs: [
         './test/specs/**/*.js'
     ],
     maxInstances: 1,
     capabilities: [{
         platformName: 'Android',
-        'appium:deviceName': 'ebac',
-        'appium:platformVersion': '9.0',
+        'appium:app': 'storage:filename=ebacshop.aab', // The filename of the mobile app
+        'appium:deviceName': 'Android GoogleAPI Emulator',
+        'appium:platformVersion': '12.0',
         'appium:automationName': 'UiAutomator2',
-        'appium:app': `${process.cwd()}/app/ebacshop.apks`,
-        'appium:appWaitActivity': '.MainActivity',
-        'appium:disableIdLocatorAutocompletion': true
-    }
-    ],
+        'appium:disableIdLocatorAutocompletion': true,
+        'sauce:options': {
+            build: 'appium-build-teste-ebacshop',
+            name: 'EBAC Shop Teste',
+            deviceOrientation: 'PORTRAIT',
+            appiumVersion: '2.0.0'
+        }
+    }],
     logLevel: 'info',
     bail: 0,
     waitforTimeout: 10000,
@@ -23,11 +34,11 @@ export const config = {
     framework: 'mocha',
     reporters: ['spec',
         ['allure', {
-                outputDir: 'allure-results',
-                disableWebdriverStepsReporting: true,
-                disableWebdriverScreenshotsReporting: false,
-            }]
-        ],
+            outputDir: 'allure-results',
+            disableWebdriverStepsReporting: true,
+            disableWebdriverScreenshotsReporting: false,
+        }]
+    ],
     mochaOpts: {
         ui: 'bdd',
         timeout: 60000
@@ -35,4 +46,4 @@ export const config = {
     afterTest: async function(test, context, { error, result, duration, passed, retries }) {
             await driver.takeScreenshot();
     }
-}
+};
